3a8aa89f3c4022b4b4a6319c8c67269959f2c1a5
Juan Romero <slayerforce880@gmail.com>
2023-03-30 09:41:37 -0600

    Exercise Upload
    JavaScript: TypeOf exercise Upload.

---
+++ Type_Of.js
@@ -0,0 +1,21 @@
+//------------TypeOf is a JavaScript operator that evaluates a parameter and returns the data type as a string------------
+
+
+//------typeof operator passed to String------
+var test = typeof("What is this")
+console.log(test)
+
+//------typeof operator  which contains a number value------
+var test = typeof(10)
+console.log(test)
+
+//------typeof operator with boolean------
+var test =  typeof(true)
+var test = typeof(false)
+var test = typeof(18>27)
+console.log(test)
+
+//------typeof operator with Object------
+var test = typeof([1,2,3])
+console.log(test)
+
